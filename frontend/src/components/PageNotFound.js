9204365152d553d737bf2769b6f3a7917b4bdd67
cheehaw01 <cheehaw01@gmail.com>
2023-12-01 17:05:44 +0800

    Minor changes: - add no match route to a Page Not Found. - rerender after data fetch.

---
+++ frontend/src/components/PageNotFound.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function PageNotFound() {
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100 pb-5 responsive-login-container bg-grey-0 overflow-hidden">
+      <div className="p-3 mb-5 rounded-3 responsive-login bg-grey-1">
+        <span
+          className="d-flex justify-content-center"
+          style={{ fontSize: 72 }}
+        >
+          😟
+        </span>
+        <h1
+          className="d-flex justify-content-center fw-bold"
+          style={{ fontSize: 64 }}
+        >
+          404
+        </h1>
+        <h1 className="d-flex justify-content-center">Page Not Found</h1>
+        <p className="d-flex justify-content-center text-center">
+          The page you are looking for doesn't exist or has been removed
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default PageNotFound;
