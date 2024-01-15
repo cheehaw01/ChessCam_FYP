3ab950843bb0bb8a16b4fbbf613ae2c94d6ab079
cheehaw01 <cheehaw01@gmail.com>
2024-01-16 00:39:57 +0800

    Update

--- backend/routes/players.js
+++ backend/routes/players.js
@@ -8,10 +8,12 @@ const {
   getPlayer,
   updatePlayer,
   deletePlayer,
+  getTopTenPlayers,
 } = require("../controllers/players");
 
 // Define routes for handling player-related operations
 router.route("/").get(getAllPlayers).post(createPlayer);
+router.route("/ranks").get(getTopTenPlayers);
 router.route("/:id").get(getPlayer).patch(updatePlayer).delete(deletePlayer);
 
 module.exports = router;
